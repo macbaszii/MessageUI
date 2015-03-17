e586d50c8ffb25975689513f0734cb2cc878f98c
Bas <bas@oozou.com>
2015-03-17 14:45:47 +0700

    Initial Commit

---
+++ SendMail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SendMail
+//
+//  Created by Kiattisak Anoochitarom on 3/17/2558 BE.
+//  Copyright (c) 2558 Kiattisak Anoochitarom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
